3044eb6fd604951ae39688ac990657febcf846ff
di1ara <dilarayalniz1@gmail.com>
2024-01-16 20:13:22 -0500

    finished palindrome_app

--- palindrome_detector/__init__.py
+++ palindrome_detector/__init__.py
@@ -1,6 +1,8 @@
 import os
 
-from flask import Flask , render_template
+from flask import Flask, render_template, request
+
+from palindrome_mhartl.phrase import Phrase
 
 
 def create_app(test_config=None):
@@ -23,15 +25,24 @@ def create_app(test_config=None):
     @app.route("/")
     def index():
         return render_template("index.html", page_title="Home")
-    
+
     @app.route("/about")
     def about():
         return render_template("about.html", page_title="About")
-    
+
     @app.route("/palindrome")
     def palindrome():
-        return render_template("palindrome.html" , page_title="Palindrome Detector")
-    
+        return render_template("palindrome.html",
+                               page_title="Palindrome Detector")
+
+    @app.route("/check", methods=("POST",))
+    def check():
+        phrase = request.form["phrase"]
+        is_palindrome = Phrase(phrase).ispalindrome()
+        return render_template("result.html",
+                                phrase=phrase,
+                                is_palindrome=is_palindrome)
+
     return app
 
 app = create_app()
